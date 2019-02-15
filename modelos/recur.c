51cfd16a4cb6f9ce80e347a194f8b3dcf5c74460
kelvinalves <32200228+kelvinalves@users.noreply.github.com>
2019-02-15 15:43:41 -0200

    recursividade em funções void

---
+++ modelos/recur.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void recursivo(int numero){
+  if(numero==0)return;
+  printf("A%d",numero);
+  recursivo(numero-1);
+  printf("B%d",numero);
+}
+int main(){
+  int numero;
+	printf("Digite um numero: ");
+	scanf("%d",&numero);
+  recursivo(numero);
+	return 0;
+}
